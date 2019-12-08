9411d47b87c3271c9466cd7c9681d9c0ccb71323
Egor <egor.kusheuv.0262@gmail.com>
2019-12-08 13:52:57 +0300

    added postgress database and simple message function on main page

---
+++ src/main/java/com/EgorProject/VSportZale/repos/MessageRepo.java
@@ -0,0 +1,7 @@
+package com.EgorProject.VSportZale.repos;
+
+import org.aspectj.bridge.Message;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+}
