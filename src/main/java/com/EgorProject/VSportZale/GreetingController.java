37a666fd2807f4cbc2fff09436b89f279f13dc47
Egor <egor.kusheuv.0262@gmail.com>
2019-12-08 17:18:36 +0300

    fixed some issues and added simple post finder

--- src/main/java/com/EgorProject/VSportZale/GreetingController.java
+++ src/main/java/com/EgorProject/VSportZale/GreetingController.java
@@ -1,12 +1,14 @@
 package com.EgorProject.VSportZale;
 
-import org.aspectj.bridge.Message;
+import com.EgorProject.VSportZale.domain.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.EgorProject.VSportZale.repos.MessageRepo;
+
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -34,5 +36,16 @@ public class GreetingController {
         model.put("messages", messages);
         return "main";
     }
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if(filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+        return "main";
 
+    }
 }
--- src/main/java/com/EgorProject/VSportZale/repos/MessageRepo.java
+++ src/main/java/com/EgorProject/VSportZale/repos/MessageRepo.java
@@ -1,7 +1,12 @@
 package com.EgorProject.VSportZale.repos;
 
-import org.aspectj.bridge.Message;
+import com.EgorProject.VSportZale.domain.Message;;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+
 }
